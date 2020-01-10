f43f65665b1e7fdd01ac0eaa94b9c54a7acb8c3f
Justin Palmer <encytemedia@gmail.com>
2020-01-09 22:40:26 -0800

    couple more changes

--- pages/top.js
+++ pages/top.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Head from 'next/head'
 import Nav from '../components/nav'
 
-const Fetch = () => (
+const Top = () => (
   <div>
     <Head>
-      <title>Fetch</title>
+      <title>Top</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <div>Hello from the Fetch root</div>
   </div>
 )
 
-export default Fetch
+export default Top
